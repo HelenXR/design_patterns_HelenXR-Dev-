b192ca64f5302cfa5c5b59ac990c555099d27bb9
HelenXR <helenhololens@gmail.com>
2017-03-07 22:49:39 +0800

    add ; for test

--- creational_patterns/singleton/source/main.cc
+++ creational_patterns/singleton/source/main.cc
@@ -6,7 +6,7 @@ using namespace std;
 
 int main(){
 	//this will cause compile error,because singleton construct is private!
-	//Singleton *singleton = new singleton();
+	//Singleton *singleton = new singleton();;
 	Singleton *singleton_p1 = Singleton::GetInstance();
 	Singleton *singleton_p2 = Singleton::GetInstance();
 	printf("singleton_p1:%p\nsingleton_p2:%p\nsingletons are the same!\n",singleton_p1,singleton_p2);
