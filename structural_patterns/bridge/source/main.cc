2d662e8a758c11216ef529599a4bf9d1ea99b7cb
HelenXR <helenhololens@gmail.com>
2017-03-10 18:28:23 +0800

    add bridge

--- structural_patterns/bridge/source/main.cc
+++ structural_patterns/bridge/source/main.cc
@@ -1,16 +1,11 @@
-/*
-优点:
-1.抽象和实现分离
-2.解决多重继承问题
-场景:
-专卖店卖鞋子,卖asics和nike跑鞋
-*/
 #include <windows.h>
 #include "nike_shoe.h"
 #include "asics_shoe.h"
 #include "helen_store.h"
 
-
+/*
+design_pattern:"bridge"
+*/
 int main(){
 	Shoe* asics_shoe = new AsicsShoe();
 	Shoe* nike_shoe = new NikeShoe();
