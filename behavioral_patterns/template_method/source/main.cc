89a091172de85b03e943602440b22b07d10510a9
HelenXR <helenhololens@gmail.com>
2017-03-21 19:39:37 +0800

    add template_method

--- behavioral_patterns/template_method/source/main.cc
+++ behavioral_patterns/template_method/source/main.cc
@@ -1,13 +1,14 @@
-/*
-跑团需要对多个学员的体能进行测试,测试内容:
-1.5公里游泳
-40公里自行车
-10公里跑步
-*/
 #include <windows.h>
 #include "zhangsan_runner.h"
 #include "lisi_runner.h"
 
+/*
+design_pattern:"template_method"
+The team needs to test the physical ability of the students,the exam contain 3 items:
+swimming
+bike
+running
+*/
 int main(){
 	Runner *zhangsan = new ZhangsanRunner();
 	Runner *lisi = new LisiRunner();
